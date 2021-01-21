2538f4baeac687c0b8ce39909ccceb6b02a9ffb0
Dirk Wangsadirdja <dirk.lingson@gmail.com>
2021-01-21 13:13:01 +0100

    Test Infer

--- scripts/models/infer_farm_konvens2020.py
+++ scripts/models/infer_farm_konvens2020.py
@@ -27,6 +27,44 @@ MODEL_DIR = BASE_DIR + "models/farm-ner-konvens2020_bert-hgcrw_direct"
 # MODEL_DIR = BASE_DIR + "models/farm-ner-konvens2020_lmgot02_direct"
 # MODEL_DIR = 'bert-base-german-cased'
 
+
+def test_file_to_dict(testfile: str = 'test', extension: str = 'txt', sep: str = '\t'):
+    filename = DATA_DIR + testfile + '.' + extension
+    with open(filename, 'r', encoding='utf-8') as fh:
+        page = fh.readlines()
+
+    text = []
+    token_list = []
+    label_list = []
+
+    tokens = []
+    for line in page[1:]:
+        temp = line.strip()
+        # Check if it's a new sentence/context
+        if temp == '':
+            text.append(' '.join(tokens))
+            tokens = []
+        else:
+            items = temp.split(sep=sep)
+            # Only first and last column is needed
+            token = items[0]
+            label = items[-1]
+            if '-' in label:
+                label = label[2:]
+
+            tokens.append(token)
+            token_list.append(token)
+            label_list.append(label)
+
+    if len(tokens) > 0:
+        text.append(' '.join(tokens))
+
+    data = []
+    for line in text:
+        data.append({"text": line})
+    return data, token_list, label_list
+
+
 def infer():
     logging.basicConfig(
         format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
@@ -38,16 +76,20 @@ def infer():
     #     {"text": "Schartau sagte dem Tagesspiegel, dass Fischer ein Idiot sei"},
     #     {"text": "Martin Müller spielt Handball in Berlin"},
     # ]
+    basic_texts, golden_list, golden_label = test_file_to_dict()
+    # pprint.pprint(basic_texts[:10])
+    # pprint.pprint(golden_list[:10])
+    # pprint.pprint(golden_label[:10])
     model = Inferencer.load(MODEL_DIR)
     # result1 = model.inference_from_dicts(dicts=basic_texts)
-    result = model.inference_from_file(DATA_DIR + 'test.txt')
+    result = model.inference_from_dicts(dicts=basic_texts)
     pprint.pprint(result[0:5])
-    # with open("test_infer1.json", 'w') as fh:
-    #    fh.write(pprint.pformat(result1, indent=2))
     with open("test_infer.json", 'w') as fh:
-        json.dump(result, fh, indent=2)
+        fh.write(pprint.pformat(result, indent=2))
+    # with open("test_infer.json", 'w') as fh:
+    #     json.dump(result, fh, indent=2)
 
-    model.close_multiprocessing_pool()
+    # model.close_multiprocessing_pool()
 
 
 if __name__ == "__main__":
