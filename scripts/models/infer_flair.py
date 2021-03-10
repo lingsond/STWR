04cd28786664550fd23d8e6175d3a735a0751739
Dirk Wangsadirdja <dirk.lingson@gmail.com>
2021-03-10 15:46:09 +0100

    try flair

---
+++ scripts/models/infer_flair.py
@@ -0,0 +1,198 @@
+import os
+import sys
+import logging
+from pathlib import Path
+from pprint import pprint
+import json
+from ast import literal_eval
+import numpy as np
+import sklearn
+from flair.data import Sentence
+from flair.models import SequenceTagger
+
+
+if os.name == 'nt':
+    BASE_DIR = "C:/My Projects/Python/STWR/"
+    use_cuda = False
+else:
+    BASE_DIR = "/home/stud/wangsadirdja/STWR/"
+    use_cuda = True
+
+DATA_DIR = BASE_DIR + "data/03_processed/Konvens2020/direct/"
+# MODEL_DIR = BASE_DIR + "models/farm-ner-konvens2020_bert-hgcrw_direct"
+# MODEL_DIR = BASE_DIR + "models/farm-ner-konvens2020_lmgot01_direct"
+# MODEL_DIR = BASE_DIR + "models/konvens2020/stwr_ner/"
+
+
+def test_file_to_dict(testfile: str = 'test', extension: str = 'txt', sep: str = '\t'):
+    # Returns 3 lists:
+    # List 1: dictionary (each line = 1 dictionary with only 'text' in the dict)
+    # List 2: all tokens (sentences are broken down to tokens)
+    # List 3: label from each token in List 2
+    filename = DATA_DIR + testfile + '.' + extension
+    with open(filename, 'r', encoding='utf-8') as fh:
+        page = fh.readlines()
+
+    text = []
+    token_list = []
+    label_list = []
+
+    tokens = []
+    for line in page[1:]:
+        temp = line.strip()
+        # Check if it's a new sentence/context
+        if temp == '':
+            text.append(' '.join(tokens))
+            tokens = []
+        else:
+            items = temp.split(sep=sep)
+            # Only first and last column is needed
+            token = items[0]
+            label = items[-1]
+            if '-' in label:
+                label = label[2:]
+
+            tokens.append(token)
+            token_list.append(token)
+            label_list.append(label)
+
+    if len(tokens) > 0:
+        text.append(' '.join(tokens))
+
+    data = []
+    for line in text:
+        data.append({"text": line})
+    return data, token_list, label_list
+
+
+def json_to_list(filename: str) -> list:
+    with open(filename, 'r', encoding='utf-8') as fh:
+        page = literal_eval(fh.read())
+
+    results = [x[0]['predictions'] for x in page]
+
+    # Separating the result if it spans over more than 1 token
+    new_results = []
+    for line in results:
+        new_line = []
+        for each in line:
+            context = each['context']
+            tokens = context.split(' ')
+            start = each['start']
+            label = each['label']
+            for token in tokens:
+                end = len(token) + start
+                new_line.append([token, start, end, label])
+                start = end + 1
+        new_results.append(new_line)
+
+    return new_results
+
+
+def text_to_list(data):
+    text_list = [x['text'] for x in data]
+
+    new_results = []
+    for line in text_list:
+        tokens = line.split(' ')
+        start = 0
+        new_line = []
+        for token in tokens:
+            end = len(token) + start
+            new_line.append([token, start, end])
+            start = end + 1
+        new_results.append(new_line)
+
+    return new_results
+
+
+def fill_prediction_list(pred, real):
+    if len(pred) != len(real):
+        raise Exception("List have different length")
+    new_preds = []
+    for n in range(len(real)):
+        preds = pred[n]
+        current = 0
+        for token in real[n]:
+            if len(preds) == 0 or current >= len(preds) or token[0] != preds[current][0]:
+                new_token = token + ['O']
+                new_preds.append(new_token)
+            else:
+                new_preds.append(preds[current])
+                current += 1
+
+    pred_list = []
+    label_list = []
+    for preds in new_preds:
+        pred_list.append(preds[0])
+        label_list.append(preds[-1])
+
+    return pred_list, label_list
+
+
+def scoring_result(xid):
+    basic_texts, golden_list, golden_label = test_file_to_dict()
+    filename = "infer_konvens2020_direct_" + xid + ".json"
+    pred_result = json_to_list(filename)
+    real_list = text_to_list(basic_texts)
+    pred_text, pred_label = fill_prediction_list(pred_result, real_list)
+
+    fscore = np.round(
+        sklearn.metrics.f1_score(pred_label, golden_label, pos_label='DIR'), 2)
+    precision = np.round(
+        sklearn.metrics.precision_score(pred_label, golden_label, pos_label='DIR'), 2)
+    recall = np.round(
+        sklearn.metrics.recall_score(pred_label, golden_label, pos_label='DIR'), 2)
+
+    print(f'FI Score : {fscore}')
+    print(f'Precision: {precision}')
+    print(f'Recall   : {recall}')
+
+
+def infer():
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        level=logging.INFO,
+    )
+    basic_texts, golden_list, golden_label = test_file_to_dict()
+
+    tagger = SequenceTagger.load('de-historic-direct')
+
+    results = []
+    texts = [x['text'] for x in basic_texts]
+    for text in texts:
+        sentence = Sentence(text)
+        tagger.predict(sentence)
+        result = model.inference_from_dicts(dicts=[text])
+        results.append(result)
+    # pprint.pprint(results)
+    filename = 'infer_konvens2020_direct_' + xid + '.json'
+    with open(filename, 'w', encoding='utf-8') as fh:
+        fh.write(pprint.pformat(results, indent=2))
+    # with open("test_infer.json", 'w') as fh:
+    #     json.dump(result, fh, indent=2)
+
+    model.close_multiprocessing_pool()
+
+
+def testing():
+    basic_texts, golden_list, golden_label = test_file_to_dict()
+    print('basic_texts')
+    #pprint(basic_texts)
+    print('golden_list')
+    # pprint(golden_list)
+    print('golden_label')
+    pprint(golden_label)
+
+
+if __name__ == "__main__":
+    # exp_id = sys.argv[1]
+    #lang_model = sys.argv[2]
+    # Parameter1 can be '', 'direct', 'indirect', 'reported'
+    # Parameter2 can be 'lmgot01', 'lmgot02', 'bert-hgcrw', 'bert-gc'
+    #ner(task, lang_model)
+    # infer(exp_id)
+    # scoring_result(exp_id)
+
+    testing()
--- scripts/models/try_flair.py
+++ scripts/models/try_flair.py
@@ -1,11 +1,57 @@
 from flair.data import Sentence
 from flair.models import SequenceTagger
+from pprint import pprint
 
-s = Sentence('I love Berlin')
-t = SequenceTagger.load('ner')
 
-t.predict(s)
+DATA_PATH = 'C:/My Projects/Python/STWR/data/03_processed/Konvens2020/direct/'
+def parse_farm_text(pfile):
+    with open(pfile, 'r', encoding='utf-8') as fh:
+        page = fh.readlines()[1:]
 
-print(s)
-for entity in s.get_spans('ner'):
-    print(entity)
+    dataset = []
+    sentence = []
+    for line in page:
+        if line.strip() == '':
+            text = ' '.join(sentence)
+            dataset.append(text)
+            sentence = []
+            continue
+
+        token = line.strip().split('\t')[0]
+        sentence.append(token)
+
+    # If last line in file is not blank row, add last sentence to dataset
+    if sentence:
+        text = ' '.join(sentence)
+        dataset.append(text)
+
+    return dataset
+
+
+def testing():
+    t = SequenceTagger.load('de-historic-direct')
+    sentences = [
+        'Ich liebe Berlin.',
+        'George Washington gang nach Washington.',
+        'Achtung!!'
+    ]
+    for item in sentences:
+        s = Sentence(item)
+        t.predict(s)
+        print(s)
+        for token in s:
+            print(token.text, token.label)
+
+    # for entity in s.get_spans('ner'):
+    #     print(entity)
+
+
+def main():
+    filename = DATA_PATH + 'test.txt'
+    sentences = parse_farm_text(filename)
+    pprint(sentences[:10])
+
+
+if __name__ == '__main__':
+    # main()
+    testing()
